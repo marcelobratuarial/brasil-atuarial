02ac2c04caa3cb35c9f494923a706007c898dd5c
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-09-14 15:18:20 -0300

    Update

--- app/Views/parts/home/testimonials.php
+++ app/Views/parts/home/testimonials.php
@@ -1,7 +1,7 @@
 
 <!--? Testimonial Start -->
 <!-- <div class="testimonial-area fix pt-180 pb-180 section-bg" data-background="<?= base_url("assets/img/gallery/section_bg03.png") ?>"> -->
-<div class="testimonial-area fix pt-180 pb-180 section-bg">
+<div class="testimonial-area fix pt-50 pb-50 section-bg">
     <div class="section-tittle text-center mb-70">
         <span>DEPOIMENTOS</span>
         <h2>Veja o que estão falando sobre nós</h2>
