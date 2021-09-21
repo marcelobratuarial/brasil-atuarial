4a6fded57af60f5b791d6d431d5c86f2fde713c7
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-09-21 10:45:14 -0300

    Depoimento André

--- app/Views/parts/home/testimonials.php
+++ app/Views/parts/home/testimonials.php
@@ -14,13 +14,11 @@
                     <div class="single-testimonial pt-65">
                         <!-- Testimonial tittle -->
                         <div class="testimonial-icon mb-45">
-                            <img src="<?= base_url("assets/img/gallery/testimonial.png") ?>" class="ani-btn " alt="">
+                            <img src="<?= base_url("assets/img/gallery/andre-saliba.jpg") ?>" class="ani-btn " alt="">
                         </div>
                             <!-- Testimonial Content -->
                         <div class="testimonial-caption text-center">
-                            <p>You can’t succeed if you just do what others do and 
-                                follow the well-worn path. You need to create a new and 
-                                original path for yourself.</p>
+                            <p>A Brasil Autuorial é uma empresa parceira a mais de 5 anos, prezamos muito por relações de longo prazo e encontramos nesta parceria uma relação de extrema confiança, onde indicamos nossos clientes que sempre nos retornam com feedbacks positivos em relação a qualidade dos serviços e do atendimento.</p>
                             <!-- Rattion -->
                             <div class="testimonial-ratting">
                                 <i class="fas fa-star"></i>
@@ -30,7 +28,7 @@
                                 <i class="fas fa-star"></i>
                             </div>
                             <div class="rattiong-caption">
-                                <span>Clifford Frazier<span> - Colorlib Themes</span> </span>
+                                <span>André Saliba<span> - COO GETRACK</span> </span>
                             </div>
                         </div>
                     </div>
