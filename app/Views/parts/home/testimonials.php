c00ac88f69fa94b1cd1dfabb102aed5e01aaa4a7
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-10-01 09:37:32 -0300

    testimonials

--- app/Views/parts/home/testimonials.php
+++ app/Views/parts/home/testimonials.php
@@ -4,34 +4,13 @@
 <div class="testimonial-area fix pt-50 pb-50 section-bg">
     <div class="section-tittle text-center mb-70">
         <span>DEPOIMENTOS</span>
-        <h2>Veja o que estão falando sobre nós</h2>
+        <h2>O que nossos clientes dizem</h2>
     </div>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-xl-8 col-lg-9 col-md-9">
                 <div class="h1-testimonial-active">
-                    <!-- Single Testimonial -->
-                    <div class="single-testimonial pt-65">
-                        <!-- Testimonial tittle -->
-                        <div class="testimonial-icon mb-45">
-                            <img src="<?= base_url("assets/img/gallery/andre-saliba.jpg") ?>" class="ani-btn " alt="">
-                        </div>
-                            <!-- Testimonial Content -->
-                        <div class="testimonial-caption text-center">
-                            <p>A Brasil Atuarial é uma empresa parceira a mais de 5 anos, prezamos muito por relações de longo prazo e encontramos nesta parceria uma relação de extrema confiança, onde indicamos nossos clientes que sempre nos retornam com feedbacks positivos em relação a qualidade dos serviços e do atendimento.</p>
-                            <!-- Rattion -->
-                            <div class="testimonial-ratting">
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                            </div>
-                            <div class="rattiong-caption">
-                                <span>André Saliba<span> - COO GETRACK</span> </span>
-                            </div>
-                        </div>
-                    </div>
+                    
                     <!-- Single Testimonial -->
                     <div class="single-testimonial pt-65">
                         <!-- Testimonial tittle -->
@@ -98,6 +77,29 @@
                             </div>
                             <div class="rattiong-caption">
                                 <span>Roney<span> - CEO LIONS</span> </span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Single Testimonial -->
+                    <div class="single-testimonial pt-65">
+                        <!-- Testimonial tittle -->
+                        <div class="testimonial-icon mb-45">
+                            <img src="<?= base_url("assets/img/gallery/andre-saliba.jpg") ?>" class="ani-btn " alt="">
+                        </div>
+                            <!-- Testimonial Content -->
+                        <div class="testimonial-caption text-center">
+                            <p>A Brasil Atuarial é uma empresa parceira a mais de 5 anos, prezamos muito por relações de longo prazo e encontramos nesta parceria uma relação de extrema confiança, onde indicamos nossos clientes que sempre nos retornam com feedbacks positivos em relação a qualidade dos serviços e do atendimento.</p>
+                            <!-- Rattion -->
+                            <div class="testimonial-ratting">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                            </div>
+                            <div class="rattiong-caption">
+                                <span>André Saliba<span> - COO GETRACK</span> </span>
                             </div>
                         </div>
                     </div>
