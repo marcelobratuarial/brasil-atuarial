fbfac32d034b63f82c251515b4369e250e24f53d
Marcelo Dênis <marcelo@agenciabrasildigital.com.br>
2021-10-04 14:09:27 -0300

    rodape

--- app/Views/layouts/main/parts/footer.php
+++ app/Views/layouts/main/parts/footer.php
@@ -42,7 +42,7 @@
                 </div>
                 <!-- Footer Menu -->
                 <div class="row d-flex justify-content-between">
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                         <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
                                 <h4>Brasil Atuarial</h4>
@@ -69,22 +69,22 @@
                         </div>
                     </div> -->
             
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                         <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
                                 <h4>CONHEÇA NOSSO GRUPO</h4>
                                 <ul>
-                                    <li><a href="https://dygo.com.br/" target="_blank">Dygo</a></li>
                                     <li><a href="https://brasilbeneficios.club/" target="_blank">Brasil Benefícios</a></li>
                                     <li><a href="https://brasilplataformas.com.br/site/" target="_blank">Brasil Plataformas</a></li>
                                     <li><a href="https://agenciabrasildigital.com.br/" target="_blank">Agência Brasil Digital</a></li>
+                                    <li><a href="https://dygo.com.br/" target="_blank">Universidade Corporativa Dygo</a></li>
                                     
                                 </ul>
                             </div>
                         </div>
                     </div>
                     
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6">
                         <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
                                 
